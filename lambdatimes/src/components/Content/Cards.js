953e42174be35863f0dbce374665062fb0b18756
Robert Driskell <driskell.ba@gmail.com>
2019-05-03 11:50:48 -0500

    Type checking implemented, Header styled components added

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const Cards = props => {
   return (
     <div className="cards-container">
           {props.cards.map(element => (
-            <Card card={element}/>
+            <Card card={element} key={element.headline}/>
           ))}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string))
+}
 
 export default Cards;
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   let tabClass = 'tab';
@@ -16,5 +17,8 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string
+}
 
 export default Tab;
