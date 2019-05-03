250b6dfe3f49dddd8d9f7805fa0bc3d69b3856d7
Robert Driskell <driskell.ba@gmail.com>
2019-05-03 11:11:41 -0500

    Cards filter

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
   let tabClass = 'tab';
-  if (props.tab == props.selectedTab) {
+  if (props.tab === props.selectedTab) {
     tabClass = 'tab active-tab';
   }
   return (
     <div
       className={tabClass}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-         props.selectTabHandler(props.tab);
-      }}
+      onClick={() => props.selectTabHandler(props.tab)}
     >
       {props.tab.toUpperCase()}
     </div>
